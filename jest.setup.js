3dc0a73b04f778c49f5a83d3d33f0b334c48530f
박건규 <geongyu09@gmail.com>
2026-07-18 17:30:47 +0900

    test: jsdom PointerEvent 폴리필 추가
    jsdom이 PointerEvent와 pointer capture API를 구현하지 않아 제스처
    테스트에서 좌표가 전달되지 않던 문제를 폴리필로 해결하였습니다.

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,24 @@
 require("@testing-library/jest-dom");
+
+// jsdom은 PointerEvent를 구현하지 않아 fireEvent.pointer*가 clientX 등 좌표를
+// 전달하지 못한다. MouseEvent를 확장한 최소 폴리필로 좌표를 흘려보낸다.
+if (typeof globalThis.PointerEvent === "undefined") {
+  class PointerEvent extends MouseEvent {
+    constructor(type, params = {}) {
+      super(type, params);
+      this.pointerId = params.pointerId ?? 0;
+      this.pointerType = params.pointerType ?? "";
+      this.isPrimary = params.isPrimary ?? false;
+    }
+  }
+  globalThis.PointerEvent = PointerEvent;
+}
+
+// jsdom에 없는 pointer capture API를 no-op으로 채워 컴포넌트 호출이 던지지 않게 한다.
+if (typeof Element !== "undefined") {
+  Element.prototype.setPointerCapture ??= function setPointerCapture() {};
+  Element.prototype.releasePointerCapture ??= function releasePointerCapture() {};
+  Element.prototype.hasPointerCapture ??= function hasPointerCapture() {
+    return false;
+  };
+}
